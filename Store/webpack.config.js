8a4e97aa06eb1eb0c3e31cf6651cd45a8cfba1d7
Igor Shah <zombee1992@gmail.com>
2022-07-12 16:07:00 +0300

    fix boots

--- Store/webpack.config.js
+++ Store/webpack.config.js
@@ -74,17 +74,3 @@ module.exports = ({ mode }) => {
 
     return merge(baseConfig, envConfig);
 };
-
-
-
-// {
-//     test: /\.s[ac]ss$/i,
-//     use: [
-//         // Creates `style` nodes from JS strings
-//         "style-loader",
-//         // Translates CSS into CommonJS
-//         "css-loader",
-//         // Compiles Sass to CSS
-//         "sass-loader",
-//     ]
-// }
