e1467e94b3d2a4cff35019b9e8cc1819e4834837
Igor Shah <zombee1992@gmail.com>
2022-07-16 18:09:43 +0300

    ui slider done!!!!!!

--- Store/src/index.ts
+++ Store/src/index.ts
@@ -16,70 +16,36 @@ import { target } from 'nouislider';
 import 'nouislider/dist/nouislider.css';
 // import wNumb from 'wnumb';
 import 'bootstrap';
+import wNumb from 'wnumb';
 // const range = document.getElementById('range') as target;
 const slider: noUiSlider.target = document.querySelector('.DaSlidaaar') as target;
 console.log(slider);
 
-// noUiSlider.create(slider, {
-//     start: [20, 80],
-//     connect: true,
-//     range: {
-//         min: 0,
-//         max: 100,
-//     },
-//     tooltips: [
-//         false, // no tooltip
-//         wNumb({ decimals: 1 }), // tooltip with custom formatting
-//         true, // tooltip with default formatting
-//     ],
-// });
+noUiSlider.create(slider, {
+    start: [20, 80],
+    connect: true,
+    range: {
+        min: 0,
+        max: 100,
+    },
+    format: {
+        // 'to' the formatted value. Receives a number.
+        to: function (value) {
+            return value + ',-';
+        },
+        // 'from' the formatted value.
+        // Receives a string, should return a number.
+        from: function (value) {
+            return Number(value.replace(',-', ''));
+        },
+    },
+    tooltips: [
+        wNumb({ decimals: 0 }), // tooltip with custom formatting
+        wNumb({ decimals: 0 }), // tooltip with default formatting
+    ],
+});
 
 // import * as data from '../products_data/product.json';
 
 // import '/node_modules/bootstrap/dist/css/bootstrap.min.css';
 // import '/node_modules/bootstrap/dist/js/bootstrap.min.js';
-// interface IData {
-//     id: number;
-//     title: string;
-//     price: number;
-//     description: string;
-//     category: string;
-//     image: string;
-//     rating: {
-//         rate: number;
-//         count: number;
-//     };
-// }
-
-// const obj: IData = {
-//     id: 1,
-//     title: 'string',
-//     price: 2,
-//     description: 'string',
-//     category: 'string',
-//     image: 'string',
-//     rating: {
-//         rate: 3,
-//         count: 4,
-//     },
-// };
-// type IData = {
-//     id: number;
-//     price: number;
-// };
-
-// const obj: IData = {
-//     id: 1,
-//     price: 2,
-// };
-
-// for (const i in obj) {
-//     console.log(i);
-//     console.log(obj[i as keyof typeof obj]);
-// }
-// console.log(obj['id']);
-// import { Tooltip, Toast, Popover } from 'boo';
-// const app = new App();
-// app.start();
-// const features = new Features();
-// features.init();
