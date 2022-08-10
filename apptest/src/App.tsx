b15b03fe519c458e90cd343988f84447b9d16f27
Igor Shah <zombee1992@gmail.com>
2022-08-10 17:36:09 +0300

    good job

--- apptest/src/App.tsx
+++ apptest/src/App.tsx
@@ -1,16 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import AboutPage from './components/shah';
-import './App.css';
+import React from "react"
+import logo from "./logo.svg"
+import AboutPage from "./components/shah"
+import "./App.css"
+import ButtonS from "./components/buttonS"
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>react shah</h1>
-        <AboutPage/>
+        <AboutPage />
+        <ButtonS />
+        <AboutPage />
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ apptest/src/components/ButtonS.tsx
@@ -0,0 +1,8 @@
+import React from "react"
+
+const ButtonS = () => {
+  let counter = 0
+  return <button onClick={() => counter++} className={'bnt bnt-primary'}></button>
+}
+
+export default ButtonS
